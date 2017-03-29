d808279f25cb7f8b196ee763429adaf2f9036486
Amriet jainandunsing <Amriet jainandunsing>
2017-03-29 11:57:49 +0200

    Changed urls back again

--- scripts/app.js
+++ scripts/app.js
@@ -5,42 +5,42 @@ movieApp.config(function ($routeProvider){
     $routeProvider
 
     .when('/', {
-        templateUrl: 'movie-spa/views/main.html',
+        templateUrl: 'views/main.html',
         controller: 'mainController'
     })
 
     .when('/register', {
-        templateUrl: 'movie-spa/views/register.html',
+        templateUrl: 'views/register.html',
         controller: 'registerController'
     })
 
     .when('/movie/:movieTitle', {
-        templateUrl: 'movie-spa/views/movie.html',
+        templateUrl: 'views/movie.html',
         controller: 'movieController'
     })
 
     .when('/admin', {
-        templateUrl: 'movie-spa/views/admin.html',
+        templateUrl: 'views/admin.html',
         controller: 'adminController'
     })
 
     .when('/loginFailed', {
-        templateUrl: 'movie-spa/views/loginFailed.html',
+        templateUrl: 'views/loginFailed.html',
         controller: 'loginController'
     })
 
     .when('/movie/:movieTitle/reviews/:reviewID', {
-        templateUrl: 'movie-spa/views/review.html',
+        templateUrl: 'views/review.html',
         controller: 'reviewController'
     })
 
     .when('/myCollections', {
-        templateUrl: 'movie-spa/views/collection.html',
+        templateUrl: 'views/collection.html',
         controller: 'createCollectionController'
     })
 
     .when('/myProfile', {
-        templateUrl: 'movie-spa/views/myProfile.html',
+        templateUrl: 'views/myProfile.html',
         controller: 'myProfileController'
     })
 
